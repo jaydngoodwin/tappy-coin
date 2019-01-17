4b0833c469dab8de7b41f1469a7f3b0297e7688a
Jaydn Goodwin <33545879+JayGG99@users.noreply.github.com>
2019-01-17 22:57:31 +0000

    Initial commit

---
+++ app/src/main/java/com/jaydngoodwin/tappycoin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jaydngoodwin.tappycoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jaydngoodwin.tappycoin.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, MainFragment.newInstance()).commitNow();
+        }
+    }
+}
